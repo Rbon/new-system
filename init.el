7826cc77ac9762c65a58d361c28d433f6bf626ba
Robin Bisho <rbon@sdf.org>
2023-08-22 21:41:52 -0700

    Created a config file to control the loading of modules.

--- init.el
+++ init.el
@@ -12,25 +12,6 @@ successful (or unnecessary) and nil if not."
                     (error-message-string err))
            nil)))
 
-(defun my/babel-load-softly (filename)
-  (condition-case err
-      (org-babel-load-file filename) 
-    (error (message "Error loading %s: \"%s\"" filename
-                    (error-message-string err))
-           nil)))
-
-
 (my/load-softly "~/.emacs.d/config/core.el")
-(org-babel-tangle-file "~/.emacs.d/config/package-manager.org")
-(my/load-softly        "~/.emacs.d/tangled/package-manager.el")
-(org-babel-tangle-file "~/.emacs.d/config/magit.org")
-(my/load-softly        "~/.emacs.d/tangled/magit.el")
-(org-babel-tangle-file "~/.emacs.d/config/elfeed.org")
-(my/load-softly        "~/.emacs.d/tangled/elfeed.el")
-(org-babel-tangle-file "~/.emacs.d/config/good-scroll.org")
-(my/load-softly        "~/.emacs.d/tangled/good-scroll.el")
-(org-babel-tangle-file "~/.emacs.d/config/org.org")
-(my/load-softly        "~/.emacs.d/tangled/org.el")
-(org-babel-tangle-file "~/.emacs.d/config/appearance.org")
-(my/load-softly        "~/.emacs.d/tangled/appearance.el")
-
+(org-babel-tangle-file "~/.emacs.d/config/modules.org")
+(my/load-softly        "~/.emacs.d/tangled/modules.el")
